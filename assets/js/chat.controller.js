0890dd434cd77ba8d92dca915d7243d2d2a72de4
William Yau <yauyinx@gmail.com>
2015-06-07 21:03:08 -0700

    chat working

--- assets/js/chat.controller.js
+++ assets/js/chat.controller.js
@@ -1,7 +1,12 @@
 var socketApp = angular.module('socketApp',[]);
-
+socketApp.config(['$interpolateProvider', function($interpolateProvider){
+  $interpolateProvider.startSymbol('[[').endSymbol(']]');
+}]);
 socketApp.controller('ChatController',['$http','$log','$scope',function($http,$log,$scope){
   io.socket.get('/chat/addconv');
+  $scope.predicate = '-id';
+  $scope.reverse = false;
+  $scope.baseUrl = 'http://localhost:1337';
 
   $http.get($scope.baseUrl+'/chat')
     .success(function(success_data){
@@ -9,4 +14,21 @@ socketApp.controller('ChatController',['$http','$log','$scope',function($http,$l
       $scope.chatList = success_data;
       $log.info(success_data);
     });
+
+  io.socket.on('chat',function(obj){
+    //Check whether the verb is created or not
+    if(obj.verb === 'created'){
+      $log.info(obj);
+      $scope.chatList.push(obj.data);
+      // Add the data to current chatList
+      // Call $scope.$digest to make the changes in UI
+      $scope.$digest();
+    }
+  });
+
+  $scope.sendMsg = function(){
+    $log.info($scope.chatMessage);
+    io.socket.post('/chat/addconv/',{user:$scope.chatUser,message: $scope.chatMessage});
+    $scope.chatMessage = "";
+  };
 }]);
