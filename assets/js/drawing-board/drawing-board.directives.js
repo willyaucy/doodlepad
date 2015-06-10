20616c1c81b93deccd0c542614f6e7bd7f5bef5f
YAUYIN <yauyinx@gmail.com>
2015-06-10 02:29:20 -0700

    initial drawing board js and css

--- assets/js/drawing-board/drawing-board.directives.js
+++ assets/js/drawing-board/drawing-board.directives.js
@@ -6,7 +6,77 @@ angular.module('MainModule')
       scope: {},
       templateUrl: '/templates/drawing-board.ejs',
       controller: ['$http','$log','$scope', function($http,$log,$scope) {
+        var context = document.getElementById('canvas').getContext("2d");
 
+        $('#canvas').mousedown(function(e){
+          var mouseX = e.pageX - this.offsetLeft;
+          var mouseY = e.pageY - this.offsetTop;
+
+          paint = true;
+          addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false);
+          redrawAll();
+        });
+        $('#canvas').mousemove(function(e){
+          if(paint){
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redrawAll();
+          }
+        });
+        $('#canvas').mouseup(function(e){
+          paint = false;
+        });
+        $('#canvas').mouseleave(function(e){
+          paint = false;
+        });
+        $('button#clearAll').click(function() {
+          var query = new Parse.Query(Stroke);
+          query.find({
+            success: function(results) {
+              results.forEach(function (result) {
+                result.destroy();
+                result.save();
+              });
+            }
+          });
+          others = {}; // stores the drawings by others (should never upload anything from here)
+          checkedX = new Array(); checkedY = new Array(); checkedDrag = new Array();
+          uploadX = new Array(); uploadY = new Array(); uploadDrag = new Array();
+        });
+        var others = {}; // stores the drawings by others (should never upload anything from here)
+        var checkedX = new Array(); var checkedY = new Array(); var checkedDrag = new Array();
+        var uploadX = new Array(); var uploadY = new Array(); var uploadDrag = new Array();
+        var paint;
+        function addClick(x, y, dragging) {
+          uploadX.push(x);
+          uploadY.push(y);
+          uploadDrag.push(dragging);
+        }
+        function draw(x, y, drag){
+          for(var i=0; i < x.length; i++) {
+            context.beginPath();
+            if(drag[i] && i){
+              context.moveTo(x[i-1], y[i-1]);
+            }else{
+              context.moveTo(x[i], y[i]);
+            }
+            context.lineTo(x[i], y[i]);
+            context.closePath();
+            context.stroke();
+          }
+        }
+        function redrawAll() {
+          context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+          context.strokeStyle = "#df4b26";
+          context.lineJoin = "round";
+          context.lineWidth = 5;
+          draw(checkedX, checkedY, checkedDrag);
+          draw(uploadX, uploadY, uploadDrag);
+          for (var sid in others) {
+            var drawing = others[sid];
+            var x=drawing.x; var y=drawing.y; var drag=drawing.drag;
+            draw(x, y, drag);
+          }
+        }
       }]
     };
   });
